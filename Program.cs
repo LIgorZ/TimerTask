d5b6ddc6a8c587be788a502e43b1246c3ecbb98f
LIgorZ <96433539+LIgorZ@users.noreply.github.com>
2022-01-02 19:20:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TimerTask
+{
+    class Program
+    {
+        public class Gamer
+        {
+            public string Name { get; set; }
+
+            public void SayGameIsOver (object sender, TimeIsOverEventArgs e)
+            {
+                Console.WriteLine($"{e.Msg} Game is over!");
+            }
+        }
+
+        public class MyAdmin
+        {
+            public string Name { get; set; }
+
+            public void SayGameIsOver (object sender, TimeIsOverEventArgs e)
+            {
+                Console.WriteLine($"{e.Msg} Time out. PC#1 is free!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var timer = new Timer { Time = 1 };
+            var artur = new Gamer { Name = "Artur" };
+            var goodAdmin = new MyAdmin { Name = "Alex"};
+            
+            timer.TimeIsOver += artur.SayGameIsOver;
+            timer.TimeIsOver += goodAdmin.SayGameIsOver;
+
+            Console.WriteLine("NET.C#.10 Делегаты и события");
+
+            timer.Start("Time of game - 1 min! ");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TimerTask
+{
+    public class TimeIsOverEventArgs: EventArgs
+    {
+        public string Msg { get; set; } 
+    }
+    class Timer
+    {
+        private int _time;
+
+        public int Time
+        {
+            get => _time;
+            
+            set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("Time must be more then zero");
+                }
+                _time = value;
+            }
+        }
+
+        public event EventHandler<TimeIsOverEventArgs> TimeIsOver = delegate
+        { };
+        private void OnTimeIsOver (TimeIsOverEventArgs eventArgs)
+        {
+            EventHandler<TimeIsOverEventArgs> temp = TimeIsOver;
+            temp?.Invoke(this, eventArgs);
+        }
+        public void Start (string msg)
+        {
+            Thread.Sleep(1000 * Time);
+            OnTimeIsOver(new TimeIsOverEventArgs() { Msg = msg});
+        }
+
+        
+    }
+}
